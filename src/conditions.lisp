9f87c4f033955fd4425a7f100d78e40a3ee6c731
David  Ochoa Corrales <david.ochoa.corrales@gmail.com>
2026-03-29 08:48:39 -0700

    new file:   src/conditions.lisp

---
+++ src/conditions.lisp
@@ -0,0 +1,83 @@
+(in-package :graph-db)
+
+(define-condition slave-auth-error (error)
+  ((reason :initarg :reason)
+   (host :initarg :host))
+  (:report (lambda (error stream)
+             (with-slots (reason host) error
+               (format stream "Slave auth error ~A: ~A." host reason)))))
+
+(define-condition transaction-error (error)
+  ((reason :initarg :reason))
+  (:report (lambda (error stream)
+             (with-slots (reason) error
+               (format stream "Transaction error: ~A." reason)))))
+
+(define-condition serialization-error (error)
+  ((instance :initarg :instance)
+   (reason :initarg :reason))
+  (:report (lambda (error stream)
+             (with-slots (instance reason) error
+               (format stream "Serialization failed for ~a because of ~a."
+                       instance reason)))))
+
+(define-condition deserialization-error (error)
+  ((instance :initarg :instance)
+   (reason :initarg :reason))
+  (:report (lambda (error stream)
+             (with-slots (instance reason) error
+               (format stream "Deserialization failed for ~a because of ~a."
+                       instance reason)))))
+
+(define-condition stale-revision-error (error)
+  ((instance :initarg :instance)
+   (current-revision :initarg :current-revision))
+  (:report (lambda (error stream)
+             (with-slots (instance current-revision) error
+               (format stream "Attempt to update stale revision ~S of ~S."
+                       instance current-revision)))))
+
+(define-condition duplicate-key-error (error)
+  ((instance :initarg :instance)
+   (key :initarg :key))
+  (:report (lambda (error stream)
+             (with-slots (instance key) error
+               (format stream "Duplicate key ~S in ~S."
+                       key instance)))))
+
+(define-condition nonexistent-key-error (error)
+  ((instance :initarg :instance)
+   (key :initarg :key))
+  (:report (lambda (error stream)
+             (with-slots (instance key) error
+               (format stream "Nonexistent key ~S in ~S."
+                       key instance)))))
+
+(define-condition node-already-deleted-error (error)
+  ((node :initarg :node))
+  (:report (lambda (error stream)
+             (with-slots (node) error
+               (format stream "Node ~A already deleted" node)))))
+
+(define-condition vertex-already-deleted-error (node-already-deleted-error)
+  ())
+
+(define-condition edge-already-deleted-error (node-already-deleted-error)
+  ())
+
+(define-condition invalid-view-error (error)
+  ((class-name :initarg :class-name)
+   (view-name :initarg :view-name))
+  (:report (lambda (error stream)
+             (with-slots (class-name view-name) error
+               (format stream
+                       "No such graph view: ~A/~A"
+                       class-name view-name)))))
+
+(define-condition view-lock-error (error)
+  ((message :initarg :message))
+  (:report (lambda (error stream)
+             (with-slots (message) error
+               (format stream
+                       "View locking error: '~A'"
+                       message)))))
